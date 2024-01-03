da20c654c296f83cd4d2904198fd63b9782425f5
Naimhz <sekki691@gmail.com>
2024-01-03 17:49:53 +0100

    .

---
+++ Backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const path = require('path');
+const helmet = require('helmet');
+
+const userRoutes = require('./routes/user');
+const bookRoutes = require('./routes/book');
+
+mongoose.connect(process.env.MONGODB_URL,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((err) => console.log('Connexion à MongoDB échouée !'+err));
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+  
+
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/auth', userRoutes);
+app.use('/api/books', bookRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
---
+++ Backend/middlewares/multer-config.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+const sharpMulter = require('sharp-multer');
+
+const MIME_TYPES = {
+	'image/jpg': 'jpg',
+	'image/jpeg': 'jpg',
+	'image/png': 'png',
+	'image/webp': 'webp',
+};
+
+const storage = sharpMulter({
+	destination: (req, file, callback) => {
+		callback(null, 'images');
+	},
+	imageOptions: {
+		fileFormat: 'webp',
+		quality: 80,
+		resize: { width: 500, height: 700, resizeMode: 'inside' },
+		useTimestamp: true,
+	},
+});
+
+const fileFilter = (req, file, callback) => {
+	if (!MIME_TYPES[file.mimetype]) {
+		callback(new Error('Veuillez passer un fichier valide'));
+	} else {
+		callback(null, true);
+	}
+};
+
+module.exports = multer({ storage: storage, fileFilter: fileFilter }).single('image');
